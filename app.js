ccdb01d38b2c141e1f2eed2cdc9b7daa836359fa
Ellie Deshaies <deshaies.elizabeth@gmail.com>
2019-08-22 22:32:18 -0600

    DOM scripting - events part 2 assignment completed

--- app.js
+++ app.js
@@ -1,46 +1,48 @@
 function onReady() {
-  const toDos = [];
+  let id = 0;
+  let toDos = [];
   const addToDoForm = document.getElementById('addToDoForm');
 
   function createNewToDo () {
     const newToDoText = document.getElementById('newToDoText');
-    if (!newToDoText.value) { return; }
-
+    if (!newToDoText.value) {
+      return;
+    }
     toDos.push({
       title: newToDoText.value,
-      complete: false
+      complete: false,
+      id: id
     });
     newToDoText.value = '';
-
+    id += 1;
     renderTheUI();
   }
 
   function renderTheUI () {
     const toDoList = document.getElementById('toDoList');
-
     toDoList.textContent = '';
-
     toDos.forEach(function(toDo) {
       const newLi = document.createElement('li');
       const checkbox = document.createElement('input');
+      const deleteButton = document.createElement('button');
       checkbox.type = "checkbox";
-
       newLi.textContent = toDo.title;
-
+      deleteButton.textContent = "Delete";
       toDoList.appendChild(newLi);
       newLi.appendChild(checkbox);
+      newLi.appendChild(deleteButton);
+      deleteButton.addEventListener('click', event => {
+        let titleDelete = toDo.id;
+        toDos = toDos.filter(e => e.id != titleDelete);
+        renderTheUI();
+      })
     });
-
   }
-
   addToDoForm.addEventListener('submit', event => {
     event.preventDefault();
     createNewToDo();
   });
-
-  renderTheUI();
 }
-
 window.onload = function() {
   onReady();
 };
